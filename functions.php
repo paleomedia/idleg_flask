bee2e978c3d8aff4b7007854adcb47042aac3cc2
Nathaniel Hoffman <nathaniel.hoffman@gmail.com>
2015-02-17 10:56:04 -0700

    First commit
    syncing files for the first time for IDLEG

---
+++ functions.php
@@ -0,0 +1,7 @@
+ outputs e.g.  somefile.txt was last modified: December 29 2002 22:16:23.
+
+$filename = 'somefile.txt';
+if (file_exists($filename)) {
+    echo "$filename was last modified: " . date ("F d Y H:i:s.", filemtime($filename));
+}
+?>
