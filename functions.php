f8087728971aea35320ab25e8c566929c9a148d2
Nathaniel Hoffman <nathaniel.hoffman@gmail.com>
2015-02-19 15:48:53 -0700

    playing with header

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
- outputs e.g.  somefile.txt was last modified: December 29 2002 22:16:23.
+<?php   // outputs e.g.  somefile.txt was last modified: December 29 2002 22:16:23.
 
-$filename = 'somefile.txt';
+$filename = 'index.html';
 if (file_exists($filename)) {
     echo "$filename was last modified: " . date ("F d Y H:i:s.", filemtime($filename));
 }
