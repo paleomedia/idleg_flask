191f069c4399d67965e7785089b8dc9c5e1aa180
paleomedia <nathaniel.hoffman@gmail.com>
2015-03-07 15:16:43 -0700

    initial table setup

---
+++ nhoffman.sql
@@ -0,0 +1,29 @@
+CREATE TABLE user (
+email VARCHAR(256) NOT NULL,
+username VARCHAR(64) NOT NULL, PRIMARY KEY
+password VARCHAR(64) NOT NULL,
+firstname VARCHAR(64),
+lastname VARCHAR(64),
+party VARCHAR(12),
+website VARCHAR(64),
+district_cong TINYINT,
+district_leg TINYINT,);
+
+
+CREATE TABLE comments (
+username VARCHAR(64),
+comment
+
+CREATE TABLE topics (
+key
+topic
+);
+
+
+CREATE TABLE bills (
+);
+
+
+CREATE TABLE lawmakers (
+
+);
